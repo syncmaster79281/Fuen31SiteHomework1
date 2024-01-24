619e94d2d2e361f1413679ca3482f1faf6a70c35
JieWunLu <syncmaster79281@gmail.com>
2024-01-24 09:11:51 +0800

    Homework2

--- Ajax/Controllers/ApiController.cs
+++ Ajax/Controllers/ApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Ajax.Models;
+using Ajax.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ajax.Controllers
@@ -11,17 +12,59 @@ namespace Ajax.Controllers
         {
             _dbContext = dbContext;
         }
-
         public IActionResult Index()
         {
-            return View();
-        }
+            //等五秒
+            System.Threading.Thread.Sleep(10000);
 
+            //return View();
+            //return Content("回傳純文字","text/plain",System.Text.Encoding.UTF8);
+            //return Content("<h2>Hello Content, 您好</h2>", "text/html", System.Text.Encoding.UTF8);
+            return Content("Hello Content, 您好!", "text/plain", System.Text.Encoding.UTF8);
+        }
+        //Get 方法
         public IActionResult Cities()
         {
             var citiers = _dbContext.Addresses.Select(a => a.City).Distinct();
             //return View();
             return Json(citiers);
+        }
+        public IActionResult Avatar(int id = 1)
+        {
+            Member? member = _dbContext.Members.Find(id);
+            if (member != null)
+            {
+                byte[] img = member.FileData;
+                return File(img, "image/jpeg");
+            }
+            return NotFound(); //404
+        }
+        public IActionResult Register(UserDto dto)
+        {
+            if (string.IsNullOrEmpty(dto.Name))
+            {
+                dto.Name = "Gust";
+            }
+            return Content($"Hello {dto.Name}, {dto.Age}歲了，電子信箱是 {dto.Email}");
+        }
+        public IActionResult Districts(string city)
+        {
+            var districts = _dbContext.Addresses.Where(a => a.City == city).Select(a => a.SiteId).Distinct();
+            return Json(districts);
+        }
+        public IActionResult Rouds(string district)
+        {
+            var rounds = _dbContext.Addresses.Where(a => a.SiteId == district).Select(a => a.Road).Distinct();
+            return Json(rounds);
+        }
+        public IActionResult CheckAccount(string name)
+        {
+            var result = _dbContext.Members.Where(m => m.Name.Contains(name)).Any();
+            if (result == false)
+            {
+                return Content("帳號可以使用", "text/plain", System.Text.Encoding.UTF8);
+            }
+            return Content("帳號已存在", "text/plain", System.Text.Encoding.UTF8);
         }
     }
 }
---
+++ Ajax/Models/ViewModels/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ajax.Models.ViewModels
+{
+    public class UserDto
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public int? Age { get; set; } = 26;
+    }
+}
